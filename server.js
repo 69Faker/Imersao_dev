4255491f37265c0ac9b3010c9bf40f8864d14362
Thiago Gamarra <thiagogamarra82@gmail.com>
2024-11-24 16:49:48 -0800

    testes
    não funcionou, começou a dar problema com banco de dados, faz o L

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-import express from "express"; // Importa o módulo Express para criar o servidor
+import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
-const app = express(); // Cria uma instância do aplicativo Express
+const app = express();
+app.use(express.static("uploads"))
+routes(app)
 
-app.use(express.static("uploads"));
-routes(app);
-
-// Configura o servidor para escutar na porta 3000 e imprime uma mensagem no console quando o servidor está pronto
-app.listen(3000, () => 
-    {
-        console.log("SERVIDOR ESCUTANDO....");
-    }
-);
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
